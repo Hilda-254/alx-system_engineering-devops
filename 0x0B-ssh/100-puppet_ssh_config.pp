1581a4352b194ce54930597a33c30baf7ad9a337
Hilda-254 <hildamunjuri@gmail.com>
2023-06-24 13:13:47 -0700

    0x0B-ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# Use Puppet to make changes to config file
+file_line { 'Don't use password to authenticate':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+
+file_line { 'Use private key':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school'
+}
